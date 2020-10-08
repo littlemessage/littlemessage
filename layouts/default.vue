ff32d371020b9829805cf43c504e11d4ffba2be6
Micah Walter <micah@micahwalter.studio>
2020-10-08 12:39:33 -0400

    added a navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <header>
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <a class="navbar-brand" href="/">Little Message</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse">
+          <span class="navbar-toggler-icon" />
+        </button>
+      </nav>
+    </header>
+
     <Nuxt />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <h1 class="title">
       Little Message
     </h1>
