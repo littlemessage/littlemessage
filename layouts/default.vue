a73f9f11180fdfd6353c03da0851a24ffdab1af1
Micah Walter <micah@micahwalter.studio>
2020-10-08 17:49:06 -0400

    login links

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,19 @@
   <div>
     <header>
       <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <a class="navbar-brand" href="/">Little Message</a>
+        <NuxtLink class="navbar-brand" to="/">
+          Little Message
+        </NuxtLink>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse">
           <span class="navbar-toggler-icon" />
         </button>
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+          <div class="form-inline mt-2 mt-md-0">
+            <NuxtLink class="btn btn-outline-primary" to="/login">
+              Log In / Sign In
+            </NuxtLink>
+          </div>
+        </div>
       </nav>
     </header>
 
