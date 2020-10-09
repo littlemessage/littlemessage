e8b499721a47ae153bec1937781c0b7ab4eedb28
Micah Walter <micah@micahwalter.studio>
2020-10-09 00:37:00 +0000

    updating nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,45 @@
 <template>
   <div>
     <header>
-      <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <NuxtLink class="navbar-brand" to="/">
-          Little Message
-        </NuxtLink>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse">
-          <span class="navbar-toggler-icon" />
-        </button>
-        <div id="navbarCollapse" class="collapse navbar-collapse">
-          <div class="form-inline mt-2 mt-md-0">
-            <NuxtLink class="btn btn-outline-primary" to="/login">
-              Log In / Sign In
-            </NuxtLink>
-          </div>
-        </div>
-      </nav>
+      <div>
+        <b-navbar toggleable="lg">
+          <b-navbar-brand href="/">
+            Little Message
+          </b-navbar-brand>
+
+          <b-navbar-toggle target="nav-collapse" />
+
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+              <b-nav-item v-if="$auth.loggedIn" href="#">
+                Link
+              </b-nav-item>
+              <b-nav-item v-if="$auth.loggedIn" href="#" disabled>
+                Disabled
+              </b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+              <b-nav-item-dropdown v-if="$auth.loggedIn" right>
+                <!-- Using 'button-content' slot -->
+                <template v-slot:button-content>
+                  <em>User</em>
+                </template>
+                <b-dropdown-item href="#">
+                  Profile
+                </b-dropdown-item>
+                <b-dropdown-item href="/logout">
+                  Sign Out
+                </b-dropdown-item>
+              </b-nav-item-dropdown>
+              <b-nav-item v-else href="/login">
+                Sign Up / Log In
+              </b-nav-item>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-navbar>
+      </div>
     </header>
 
     <Nuxt />
